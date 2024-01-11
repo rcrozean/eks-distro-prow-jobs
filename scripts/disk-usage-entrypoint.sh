02efefb3253e77d8eb09b6c85eb2727e737e937e
Cameron Rozean <rcrozean@amazon.com>
2024-01-11 12:33:18 -0800

    update scripts and presets to pull configs

--- scripts/disk-usage-entrypoint.sh
+++ scripts/disk-usage-entrypoint.sh
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+#
 # Run registry in background mode to be able to poll
 # for the done status file and kill process after
 
